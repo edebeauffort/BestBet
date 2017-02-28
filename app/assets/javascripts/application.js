4ec8efae55c9b29d09d8e4af312ab932087ec50f
antonio lima <a.m.maiadelima@gmail.com>
2017-02-28 12:40:40 +0000

    finished fluid homepage beahaviour

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,12 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+$(function(){
+
+  $(".fake-btn").on("click", function(e){
+    $(".fake-div").removeClass('hidden');
+    $(this).addClass('hidden');
+    $(".real-submit").removeClass('hidden');
+  });
+
+});
