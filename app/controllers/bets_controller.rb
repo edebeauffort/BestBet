a3d409798c4b64b3a48813170eab0ede6e32b1c9
Nicolai Wiskemann <nicolai.wiskemann@gmail.com>
2017-02-28 15:04:30 +0000

    hu

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,11 +1,36 @@
+
 class BetsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :show ]
+  skip_before_action :authenticate_user!, only: [ :show, :create]
+  def index
+
+    @pool = Pool.find(params[:pool_id])
+    @selection = Selection.find(params[:selection_id])
+    @user = User.find(params[:user_id])
+  end
+  def new
+    @pool = Pool.find(params[:pool_id])
+    @selection = Selection.find(params[:selection_id])
+    @user = User.find(params[:user_id])
+    @bet = Bet.new
+  end
+
+  def create
+    @selection = Selection.find(params[:selection_id])
+    @user = current_user
+    @bet = Bet.new(user_id: @user.id, selection_id: @selection.id)
+    if @bet.save
+     redirect_to bets_path
+   else
+      redirect_to root_path
+    end
+  end
+
   def show
     @bet = Bet.find(params[:id])
-
     @pool = Pool.find(params[:id])
     @selection = Selection.find(params[:id])
     @user = User.find(params[:id])
   end
+  private
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :pools, only: [:create,:new,:show]
   resources :selections, only: [:create]
-resources :bets, only: [:create, :show]
+resources :bets, only: [:create, :new, :show, :index]
 
   resources :pools, only: [:new, :create]
   resources :selections, only: [:create]
