859b074962f5c4e4db3c0d6368440d8b6df99a53
Elissa Besta <elissa.besta@gmail.com>
2017-03-01 17:16:12 +0000

    add confirm winner

--- app/assets/javascripts/confirm_winner.js
+++ app/assets/javascripts/confirm_winner.js
@@ -1,20 +1,20 @@
-angular.module('ionicApp', ['ionic'])
+// angular.module('ionicApp', ['ionic'])
 
-.controller('MainCtrl', function($scope) {
+// .controller('MainCtrl', function($scope) {
 
-    $scope.itens = [
-        { title: "Item 1", checked: false },
-        { title: "Item 2", checked: false },
-        { title: "Item 3", checked: false },
-    ];
+//     $scope.itens = [
+//         { title: "Item 1", checked: false },
+//         { title: "Item 2", checked: false },
+//         { title: "Item 3", checked: false },
+//     ];
 
-    $scope.updateSelection = function(position, itens, title) {
-        angular.forEach(itens, function(subscription, index) {
-            if (position != index)
-                subscription.checked = false;
-                $scope.selected = title;
-            }
-        );
-    }
+//     $scope.updateSelection = function(position, itens, title) {
+//         angular.forEach(itens, function(subscription, index) {
+//             if (position != index)
+//                 subscription.checked = false;
+//                 $scope.selected = title;
+//             }
+//         );
+//     }
 
-});
+// });
--- app/models/pool.rb
+++ app/models/pool.rb
@@ -4,4 +4,5 @@ class Pool < ApplicationRecord
   has_many :selections
   has_many :bets
   accepts_nested_attributes_for :selections, reject_if: :all_blank, allow_destroy: true
+  belongs_to :winning_selection, optional: :true, class_name: "Selection", foreign_key: :selection_id
 end
